f897383f7350b81e31b8fe615557b6d662141ada
duane-faria <duane.faria@etec.sp.gov.br>
2020-09-05 13:42:05 -0300

    .

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ class App {
     this.server.use(cors());
     this.server.use(
       '/files',
-      express.static(resolve(__dirname, '..', 'public', 'uploads'))
+      express.static(resolve(__dirname, '..', 'public'))
     );
   }
 
